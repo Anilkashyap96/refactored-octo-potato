6799794fc9af1dbdff7a3ec46241d99717134660
Anil Kashyap <anil.kashyap@toprankers.co.in>
2020-06-20 06:56:33 +0530

    fetching api using react

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -49,23 +49,26 @@ import './App.css';
 // }
 
 class App extends React.Component{
-  state = {
-    title: "Suresh",
-    status: "Programmer"
-  }
-  changeName = () =>{
-    this.setState({
-      status: "web developer"
-    })
-  }
 
+  state={
+    title: "Anil Kashyap"
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(res => res.json())
+    .then(res2 => {
+      console.log(res2)
+      this.setState({
+        title:res2[0].title
+      })
+    })
+
+  }
   render(){
-    console.log(this.state.status);
+    
     return(
       <div className="App">
-        <h1>{this.state.title}</h1>
-        <h2>{this.state.status}</h2>
-        <button onClick={()=> this.changeName()}>Click me</button>
+          <h1>{this.state.title}</h1>    
       </div>
     );
   }
